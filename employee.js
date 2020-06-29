0106c3bcfc4f0f1ddf5a474e50ab19f5655480fa
Jorge Guzman <jorguzman100@gmail.com>
2020-06-29 18:19:33 -0500

    Query class with Async queries in the CRUD

--- employee.js
+++ employee.js
@@ -3,11 +3,22 @@ const inquirer = require("inquirer");
 const Query = require("./lib/query");
 
 
-// CLI
-const startCMS = () => {
-    // Display all employee DB
-    const queryAll = new Query('Read', 'employee', '*');
-    queryAll.runQuery();
+// CLI 
+// constructor (crud, table, object, ...columns)
+const startCMS = async () => {
+    const query2 = await new Query('Create', 'employee', {
+                    first_name: "2nd test",
+                    last_name: "Man",
+                    role_id: 1,
+                    manager_id: 4
+                }, '*').builQuery();
+    displayAllEmployees();
 }
+
+const displayAllEmployees = () => {
+    // Display all employee DB
+    const query1 = new Query('Read', 'employee', {}, '*').builQuery();
+}
+
 
 startCMS();
