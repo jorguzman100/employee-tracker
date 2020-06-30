e56a920e8ee7e79ec46184ecd79d6d4f3f5ed2db
Jorge Guzman <jorguzman100@gmail.com>
2020-06-29 19:25:38 -0500

    CRUD Delete query created

--- employee.js
+++ employee.js
@@ -16,7 +16,11 @@ const startCMS = async () => {
     }], '*').buildQuery();
 
     // U
-    const query3 = await new Query('Update', 'employee', [{first_name: 'Maggi'}, {first_name: '2nd test'}]).buildQuery();
+    const query3 = await new Query('Update', 'employee', [{ first_name: 'Maggi' }, { first_name: '2nd test' }]).buildQuery();
+
+
+    // D
+    const query4 = await new Query('Delete', 'employee', [{ first_name: 'Maggi' }]).buildQuery();
 
     displayAllEmployees();
 }
