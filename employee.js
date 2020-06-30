acbc73d6e4920245350534bc29676fc46e55339f
Jorge Guzman <jorguzman100@gmail.com>
2020-06-29 20:25:52 -0500

    Select CRUD and select Table inquirer prompts working

--- employee.js
+++ employee.js
@@ -5,30 +5,70 @@ const Query = require("./lib/query");
 
 // CLI 
 // constructor (crud, table, options, ...columns)
-const startCMS = async () => {
 
-    // C
-    const query2 = await new Query('Create', 'employee', [{
-        first_name: "2nd test",
-        last_name: "Man",
-        role_id: 1,
-        manager_id: 4
-    }], '*').buildQuery();
-
-    // U
-    const query3 = await new Query('Update', 'employee', [{ first_name: 'Maggi' }, { first_name: '2nd test' }]).buildQuery();
-
-
-    // D
-    const query4 = await new Query('Delete', 'employee', [{ first_name: 'Maggi' }]).buildQuery();
-
-    displayAllEmployees();
+const selectCRUD = () => {
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'crud',
+            message: 'What action do you want to take?',
+            choices: ['Create', 'Read', 'Update', 'Delete', 'Exit'],
+        }
+    ]).then((answer) => {
+        console.log(answer.crud);
+        selectTable(answer.crud);
+    });
 }
 
-const displayAllEmployees = () => {
+const selectTable = (crud) => {
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'table',
+            message: 'What table do you want to work with?',
+            choices: ['Employees', 'Roles', 'Departments', 'Exit'],
+        }
+    ]).then((answer) => {
+        console.log(answer.table);
+        selectOptions(crud, answer.table);
+    });
+}
+
+const selectOptions = (crud, table) => {
+    console.log('In selectOptions()');
+    console.log(`crud: ${crud}, table: ${table}`);
+}
+
+const displayAllEmployees = async () => {
     // R - Display all employee DB
-    const query1 = new Query('Read', 'employee', [{}], '*').buildQuery();
+    const query1 = await new Query('Read', 'employee', [{}], '*').buildQuery();
+
+    // Define the CRUD action
+    selectCRUD();
 }
 
-startCMS();
+displayAllEmployees();
+
+
+/* 
+
+// C
+const query2 = await new Query('Create', 'employee', [{
+    first_name: "2nd test",
+    last_name: "Man",
+    role_id: 1,
+    manager_id: 4
+}], '*').buildQuery();
+
+// U
+const query3 = await new Query('Update', 'employee', [{ first_name: 'Maggi' }, { first_name: '2nd test' }]).buildQuery();
+
+
+// D
+const query4 = await new Query('Delete', 'employee', [{ first_name: 'Maggi' }]).buildQuery();
+
+ */
+
+
+
 
