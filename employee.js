f2d1e69b7b1d03c0471727462d9d62fa45daf90e
Jorge Guzman <jorguzman100@gmail.com>
2020-06-30 10:11:36 -0500

    UPDATE employees working

--- employee.js
+++ employee.js
@@ -38,6 +38,8 @@ const selectOptions = async (crud, table) => {
 
     // C
     let fields;
+    let where = '';
+    let updateAnswer;
     if (crud === 'Create' && table === 'Employees') {
         fields = await promptEmployeesFields();
     } else if (crud === 'Create' && table === 'Roles') {
@@ -51,9 +53,64 @@ const selectOptions = async (crud, table) => {
         fields = {};
     }
 
-    console.log(fields);
-    const query2 = await new Query(crud, table, [{ fields }], '*').buildQuery();
+    // U
+    if (crud === 'Update' && table === 'Employees') {
+        updateAnswer = await promptUpdateEmployeesFields();
+        fields = updateAnswer.fields;
+        where = updateAnswer.where;
+    } else if (crud === 'Update' && table === 'Roles') {
+        // fields = await promptRolesFields();
+    } else if (crud === 'Update' && table === 'Departments') {
+        // fields = await promptDepartmentsFields();
+    }
+
+    const query2 = await new Query(crud, table, [ fields ], where, '*').buildQuery();
 }
+
+const promptUpdateEmployeesFields = async () => {
+    let answer1 = await inquirer.prompt([
+        {
+            type: 'input',
+            name: 'employee_id',
+            message: 'employee_id: '
+        }
+    ]);
+    const query3 = await new Query('Read Where', 'Employees', [{ employee_id: answer1.employee_id }], '', '*').buildQuery();
+    console.log('query3: ', query3);
+    console.log('first_name: ', query3[0].first_name);
+    let answer2 = await inquirer.prompt([
+        {
+            type: 'input',
+            name: 'first_name',
+            message: 'first_name:',
+            default: query3[0].first_name
+        },
+        {
+            type: 'input',
+            name: 'last_name',
+            message: 'last_name:',
+            default: query3[0].last_name
+        },
+        {
+            type: 'input',
+            name: 'role_id',
+            message: 'role_id:',
+            default: query3[0].role_id
+        },
+        {
+            type: 'input',
+            name: 'manager_id',
+            message: 'manager_id:',
+            default: query3[0].manager_id
+        }
+    ]);
+    return { where: `employee_id=${answer1.employee_id}`, fields: answer2}
+}
+
+// U
+/* const query3 = await new Query('Update', 'Employees', [{ first_name: 'Maggi' }, { first_name: '2nd test' }]).buildQuery(); */
+
+
 
 const promptDepartmentsFields = async () => {
     return await inquirer.prompt([
@@ -112,7 +169,7 @@ const promptEmployeesFields = async () => {
 
 const displayAllEmployees = async () => {
     // R - Display all employee DB
-    const query1 = await new Query('Read', 'Employees', [{}], '*').buildQuery();
+    const query1 = await new Query('Read', 'Employees', [{}], '', '*').buildQuery();
 
     // Define the CRUD action
     selectCRUD();
@@ -122,10 +179,6 @@ displayAllEmployees();
 
 
 /*
-
-// U
-const query3 = await new Query('Update', 'Employees', [{ first_name: 'Maggi' }, { first_name: '2nd test' }]).buildQuery();
-
 
 // D
 const query4 = await new Query('Delete', 'Employees', [{ first_name: 'Maggi' }]).buildQuery();
